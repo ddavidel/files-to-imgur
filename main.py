70ef9b0470c49a119044d3a2ae9e900712930430
Davide <dvdlavezzari@gmail.com>
2024-09-08 14:50:39 +0200

    Improved main with exception handling and args detection in calling command

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 Using Imgur as free cloud storage
 """
 import sys
+from alive_progress import alive_bar
 
 import src.logger as logs
 from src.database import DataBase
 from src.filereader import FileReader
-from src.filewriter import FileWriter
+from src.imagewriter import FileWriter
 
 
 logger = logs.Logger()
@@ -18,10 +19,43 @@ if len(sys.argv) <= 1:
     logger.log('No file specified.', status=logs.FATAL)
     sys.exit()
 
-input_filename = sys.argv[1]
-logger.log(f'Found filename in args: {input_filename}')
+# Beginning here
+logger.log('Starting main process')
+try:
+    # Collect settings:
+    calling_args = sys.argv[1:]
+    filenames = []
+    color_mode = 'L'
+    skip_next = False
+    for i, arg in enumerate(calling_args):
+        if skip_next:
+            skip_next = False
+            continue
+        if arg in ('-m', '--mode'):
+            color_mode = calling_args[i + 1]
+            skip_next = True
+        else:
+            filenames.append(arg)
 
-input_file_data = filereader.read(input_filename)
+    with alive_bar(len(filenames)) as abar:
+        for input_filename in filenames:
+            abar()
+            logger.log(f'Working on {input_filename}...')
 
-db.close()
-logger.close()
+            # Reading input file
+            input_file_data = filereader.read(input_filename)
+
+            # getting filename like this is not the best but who cares
+            new_filename = input_filename.split('.')[0]
+            # Writing image
+            image_file = filewriter.write(
+                byte_data=input_file_data,
+                filename=new_filename,
+                mode=color_mode,
+            )
+except Exception as e:
+    logger.log('An error occurred while running.', status=logs.FATAL)
+    logger.log(f'{e}', status=logs.FATAL)
+finally:
+    db.close()
+    logger.close()
